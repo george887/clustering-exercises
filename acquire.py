3b61613f188536bd3cfeab63c3432d4d76b138a6
George Arredondo <george.arredondo87@gmail.com>
2020-10-10 17:05:18 -0500

    adding acquire

---
+++ acquire.py
@@ -0,0 +1,56 @@
+import pandas as pd
+# ENV has credentials needed to access the SQL database. Gitignore should be created before to prevent your info to be compromised
+from env import host, user, password
+# OS Looks to see if data is stored locally
+import os
+# allows us to ignore any warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+########################## Establishing connection ###########################
+# establish mysql connection
+def get_connection(db, user=user, host=host, password=password):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+########################## Creating function to get data ######################
+def new_zillow_data():
+    '''
+    This function reads the zillow data from the Codeup db into a df,
+    write it to a csv file, and returns the df. 
+    '''
+    # Selecting all data in the properties_2017 table
+    sql_query = '''SELECT * FROM properties_2017 as prop
+                LEFT JOIN predictions_2017 as pred on prop.id = pred.id
+                LEFT JOIN propertylandusetype as pluy on prop.propertylandusetypeid = pluy.propertylandusetypeid
+                WHERE transactiondate between '2017-01-01' and '2017-12-31'
+                '''
+
+    # The pandas read_sql function allows us to create a df with the afformentioned sql querry    
+    df = pd.read_sql(sql_query, get_connection('zillow'))
+
+    # Converts the df into a csv
+    df.to_csv('zillow_df.csv')
+
+    # This prevents any duplicated columns. The ~ allows to return the unique columns. A boolean array is created
+    # and only falses are returned
+    df = df.loc[:,~df.columns.duplicated()]
+
+    return df
+
+def get_zillow_data(cached=False):
+    '''
+    This function reads in zillow data from Codeup database if cached == False, a csv is created
+    returning the df. If cached == True, the function reads in the zillow df from a csv file & returns df
+    '''
+    # This runs if there is no csv containing the zillow data
+    if cached or os.path.isfile('zillow_df.csv') == False:
+
+        # Converts the df into a csv
+        df = new_zillow_data()
+
+    else:
+
+        # If the csv was stored locally, the csv will return the df
+        df = pd.read_csv('zillow_df.csv', index_col=0)
+
+    return df
